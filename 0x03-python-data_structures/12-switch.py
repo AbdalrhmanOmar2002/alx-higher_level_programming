17c82ad403e46df8e251b2d7c0cfc0044ef49d78
AbdalrhmanOmar2002 <witid2002@gmail.com>
2024-01-11 20:04:40 +0200

    all done

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
 a = 89
 b = 10
-c, a, b = a, b, a
+c = b  # Store the original value of b in c
+b = a  # Assign the value of a to b
+a = c  # Assign the original value of b (now in c) to a
 print("a={:d} - b={:d}".format(a, b))
